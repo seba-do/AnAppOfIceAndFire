881ad8ffef081adf15bb903929ce3b84edb4b7da
Sebastian Domma <sebastian.domma@chrono24.com>
2022-04-19 02:16:15 +0200

    Update Theme

--- app/src/main/java/com/ttdrp/gameofthrones/ui/theme/Color.kt
+++ app/src/main/java/com/ttdrp/gameofthrones/ui/theme/Color.kt
@@ -2,7 +2,29 @@ package com.ttdrp.gameofthrones.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val White = Color.White
+val Black = Color.Black
+val Brand = Color(0xFF04293A)
+val BrandHigh = Color(0xFF13455C)
+val Highlight = Color(0xFFC39052)
+val HighLightLow = Color(0xFF896C42)
+val Text = Color(0xFF03181C)
+val TextLink = Color(0xFF2683AE)
+val TextLinkHigh = Color(0xFF1A92C8)
+val Link = Color(0xFF81949C)
+val Line = Color(0xFFCDD4D8)
+val LineInverted = Color(0xFF294C5E)
+val SubDefault = Color(0xFFCFCFCF)
+val SubLight = Color(0xFFE6EAEC)
+val SubHigh = Color(0xFFF2F4F5)
+val SubLow = Color(0xFF868686)
+val Success = Color(0xFF51A700)
+val Trusted = Color(0xFF23A15C)
+val TrustedLow = Color(0xFF1B7D47)
+val TrustedIcon = Color(0xFF187543)
+val TrustedBg = Color(0xFFE4F4EC)
+val BrandBg = Color(0xFFE8F4F9)
+val Warning = Color(0xFFE89508)
+val Alert = Color(0xFFB30000)
+val AlertLow = Color(0xFFD76A64)
+val AlertHigh = Color(0xFFDC0404)
--- app/src/main/java/com/ttdrp/gameofthrones/ui/theme/Theme.kt
+++ app/src/main/java/com/ttdrp/gameofthrones/ui/theme/Theme.kt
@@ -6,28 +6,41 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+private val LightColorPalette = lightColors(
+    primary = Brand,
+    primaryVariant = BrandHigh,
+    secondary = Highlight,
+    secondaryVariant = HighLightLow,
+    background = White,
+    surface = White,
+    error = Alert,
+    onPrimary = White,
+    onSecondary = White,
+    onBackground = Text,
+    onSurface = Text,
+    onError = White
 )
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+private val DarkColorPalette = darkColors(
+    primary = Brand,
+    primaryVariant = BrandHigh,
+    secondary = Highlight,
+    secondaryVariant = HighLightLow,
+    background = Text,
+    surface = Text,
+    error = Alert,
+    onPrimary = White,
+    onSecondary = White,
+    onBackground = White,
+    onSurface = White,
+    onError = White
 )
 
 @Composable
 fun GameOfThronesTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+    val colors = if (isSystemInDarkTheme()) DarkColorPalette else LightColorPalette
 
     MaterialTheme(
         colors = colors,
