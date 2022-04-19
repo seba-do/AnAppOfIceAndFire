4657c0be61135d3d0e150f5041056cdedb456191
Sebastian Domma <sebastian.domma@chrono24.com>
2022-04-19 02:15:49 +0200

    Add HouseDivider

---
+++ app/src/main/java/com/ttdrp/gameofthrones/ui/houses/elements/HouseDivider.kt
@@ -0,0 +1,16 @@
+package com.ttdrp.gameofthrones.ui.houses.elements
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HouseListDivider() {
+    Divider(
+        modifier = Modifier.padding(horizontal = 14.dp),
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.08f)
+    )
+}
